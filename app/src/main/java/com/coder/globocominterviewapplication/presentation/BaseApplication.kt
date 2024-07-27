a4e4a7006f016efe00d6178c0ca18ef98775016f
Nisha Khatoon <786ariba7233@gmail.com>
2024-07-27 18:24:56 +0530

    commit baseclass into main

---
+++ app/src/main/java/com/coder/globocominterviewapplication/presentation/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.coder.globocominterviewapplication.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
